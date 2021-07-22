97dcaafbe6f57ca528362c519c63a706297f2c97
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2021-07-23 01:40:56 +0430

    add pieces on chessboard

--- include/ChessBoard.hpp
+++ include/ChessBoard.hpp
@@ -12,17 +12,18 @@ class ChessBoard
 public:
     ChessBoard(const ChessBoard &) = delete;     // delete copy constructor (because of singleton)
     void operator=(const ChessBoard &) = delete; // delete operator= (because of singleton)
-    
-    static ChessBoard &make_chess_board();       // a static function that can creat and return only one object of ChessBoard class (singleton method)
-    void movePiece(std::string);                 // a function that enter a move after validation it
+    ~ChessBoard();                               // destructor
 
-    bool is_attack(const int&, const int&);      //checking the move is an attack or not
-    Score attack(const int&, const int&);         //attacks and returns the score of attack (not complete yet)
+    static ChessBoard &make_chess_board(); // a static function that can creat and return only one object of ChessBoard class (singleton method)
+    void movePiece(std::string);           // a function that enter a move after validation it
+
+    bool is_attack(const int &, const int &); //checking the move is an attack or not
+    Score attack(const int &, const int &);   //attacks and returns the score of attack (not complete yet)
 
 private:
-    ChessBoard();                                // private constructor
-    std::array<std::array<Cell, 8>, 8> cells;    //a 2D array of Cell for 64 cells of chess board
-    std::array<Player*, 2> player;               //index 0 for player with white pieces and 1 for black
+    ChessBoard();                             // private constructor
+    std::array<std::array<Cell, 8>, 8> cells; //a 2D array of Cell for 64 cells of chess board
+    std::array<Player *, 2> player;           //index 0 for player with white pieces and 1 for black
 };
 
 #endif // CHESSBOARD_HPP
--- src/ChessBoard.cpp
+++ src/ChessBoard.cpp
@@ -1,4 +1,10 @@
 #include "ChessBoard.hpp"
+#include "Rook.hpp"
+#include "Knight.hpp"
+#include "Bishop.hpp"
+#include "Queen.hpp"
+#include "King.hpp"
+#include "Pawn.hpp"
 #include "chessman.hpp"
 #include <string>
 #include <iostream>
@@ -7,10 +13,60 @@ using namespace std;
 ChessBoard &ChessBoard::make_chess_board()
 {
     static ChessBoard board; // make a static object of the ChessBoard class (because of singleton)
+
+    // set black pieces on board
+    board.cells[0][0].set_piece(new Rook(Chessman::color_::Black));
+    board.cells[0][1].set_piece(new Knight(Chessman::color_::Black));
+    board.cells[0][2].set_piece(new Bishop(Chessman::color_::Black));
+    board.cells[0][3].set_piece(new Queen(Chessman::color_::Black));
+    board.cells[0][4].set_piece(new King(Chessman::color_::Black));
+    board.cells[0][5].set_piece(new Bishop(Chessman::color_::Black));
+    board.cells[0][6].set_piece(new Knight(Chessman::color_::Black));
+    board.cells[0][7].set_piece(new Rook(Chessman::color_::Black));
+
+    board.cells[1][0].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][1].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][2].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][3].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][4].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][5].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][6].set_piece(new Pawn(Chessman::color_::Black));
+    board.cells[1][7].set_piece(new Pawn(Chessman::color_::Black));
+
+    // set white pieces on board
+    board.cells[7][0].set_piece(new Rook(Chessman::color_::White));
+    board.cells[7][1].set_piece(new Knight(Chessman::color_::White));
+    board.cells[7][2].set_piece(new Bishop(Chessman::color_::White));
+    board.cells[7][3].set_piece(new Queen(Chessman::color_::White));
+    board.cells[7][4].set_piece(new King(Chessman::color_::White));
+    board.cells[7][5].set_piece(new Bishop(Chessman::color_::White));
+    board.cells[7][6].set_piece(new Knight(Chessman::color_::White));
+    board.cells[7][7].set_piece(new Rook(Chessman::color_::White));
+
+    board.cells[6][0].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][1].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][2].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][3].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][4].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][5].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][6].set_piece(new Pawn(Chessman::color_::White));
+    board.cells[6][7].set_piece(new Pawn(Chessman::color_::White));
+
     return board;
 }
 
 ChessBoard::ChessBoard() {} // constructor
+
+ChessBoard::~ChessBoard()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            delete cells[i][j].get_piece();
+        }
+    }
+}
 
 void ChessBoard::movePiece(string move)
 {
@@ -20,7 +76,6 @@ void ChessBoard::movePiece(string move)
 
     int x_second = 8 - (move[4] - 48); // calculate the cells array index that piece wants to go
     int y_second = move[3] - 65;
-
 
     if ((cells[x_first][y_first].get_piece())->cell_access(x_first, y_first, x_second, y_second, cells))
     {
@@ -33,21 +88,21 @@ void ChessBoard::movePiece(string move)
     }
 }
 
-bool ChessBoard::is_attack(const int& x_destination, const int& y_destination)
+bool ChessBoard::is_attack(const int &x_destination, const int &y_destination)
 {
     //no need to check colors (colors has checked in cell access)
     if (cells[x_destination][y_destination].is_fill())
     {
         return true;
     }
-    
+
     return false;
 }
 
-Score ChessBoard::attack(const int& x, const int& y)
+Score ChessBoard::attack(const int &x, const int &y)
 {
     Score score = 0;
-    
+
     switch (cells[x][y].get_piece()->get_symbol())
     {
     case 'P':
@@ -59,7 +114,7 @@ Score ChessBoard::attack(const int& x, const int& y)
     case 'B':
         score += 8;
         break;
-        
+
     case 'Q':
         score += 15;
         break;
